944ff7605f1406d900b3afadeae79adb16ed787d
sarita <adhsarita24@gmail.com>
2022-11-23 17:32:00 -0600

    backend finished

--- src/main/java/com/example/employemanager/EmployeeController.java
+++ src/main/java/com/example/employemanager/EmployeeController.java
@@ -27,21 +27,21 @@ public class EmployeeController
 		{
 			this.employeeService = employeeService;
 		}
-		@GetMapping("/all")
+		@GetMapping(path="/all")
 		public ResponseEntity<List<Employee>> getAllEmployees()
 		{
 			List<Employee>employees = employeeService.findAllEmployees();
 			return new ResponseEntity<>(employees, HttpStatus.OK);
 		}
 		
-		@GetMapping("/find/{id}")
+		@GetMapping(path ="/find/{id}")
 		public ResponseEntity<Employee> getEmployeeById(@PathVariable("id")Long id)
 		{
 			Employee employee = employeeService.findEmployeeById(id);
 			return new ResponseEntity<>(employee, HttpStatus.OK);
 		}
 		
-		@PostMapping("/add")
+		@PostMapping(path ="/add")
 		public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee)
 		{
 			Employee newEmployee = employeeService.addEmployee(employee);
@@ -49,14 +49,14 @@ public class EmployeeController
 			
 		}
 		
-		@PutMapping("/update")
+		@PutMapping(path ="/update")
 		public ResponseEntity<Employee> updateEmployee(@RequestBody Employee employee)
 		{
 			Employee updateEmployee = employeeService.updateEmployee(employee);
 			return new ResponseEntity<>(updateEmployee, HttpStatus.OK);
 			
 		}
-		@DeleteMapping("/delete/{id}")
+		@DeleteMapping(path="/delete/{id}")
 		public ResponseEntity<?> deleteEmployee(@PathVariable("id")Long id)
 		{
 			employeeService.deleteEmployee(id);
--- src/main/java/com/example/employemanager/repo/EmployeeRepo.java
+++ src/main/java/com/example/employemanager/repo/EmployeeRepo.java
@@ -2,13 +2,16 @@ package com.example.employemanager.repo;
 
 import java.util.Optional;
 
+
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.employemanager.model.Employee;
 
 public interface EmployeeRepo extends JpaRepository<Employee, Long>
 {
-
+	@Transactional
 	void deleteEmployeeById(Long id);
 
 	Optional<Employee> findEmployeeById(Long id);
